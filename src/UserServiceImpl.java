fbf0c5b48847881a4304fc5306ed258bf7ec0a08
nicoros <251570494@qq.com>
2020-12-23 16:01:00 +0800

    sssss

---
+++ src/UserServiceImpl.java
@@ -0,0 +1,16 @@
+/**
+ * @author chenhz
+ * @create 2020/12/23 15:59
+ */
+public class UserServiceImpl implements UserService{
+
+    @Override
+    public void add() {
+
+    }
+
+    @Override
+    public void delete() {
+
+    }
+}
