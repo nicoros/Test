2d4bc20eeda4da33c528fa36b78136159b225e49
nicoros <251570494@qq.com>
2020-12-23 14:57:01 +0800

    sss

---
+++ src/User.java
@@ -0,0 +1,43 @@
+/**
+ * @author chenhz
+ * @create 2020/12/23 14:46
+ */
+public class User {
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    private Integer id;
+    private String name;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    private Integer age;
+}
