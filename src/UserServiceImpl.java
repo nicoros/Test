3735df1e3398210337f8ae95e21536ba789aef92
nicoros <251570494@qq.com>
2020-12-28 01:29:21 +0800

    sssss

--- src/UserServiceImpl.java
+++ src/UserServiceImpl.java
@@ -6,11 +6,11 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public void add() {
-
+        System.out.println("add");
     }
 
     @Override
     public void delete() {
-
+        System.out.println("delete");
     }
 }
